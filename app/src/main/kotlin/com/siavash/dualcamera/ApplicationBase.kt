41cd01ede66f3a6032febcef09743bf1f54b0493
Siavash Kavousi <siavsah.kavousi.sk@gmail.com>
2015-12-29 17:59:46 +0330

    Makes crashlytics kit disabled in debug mode

--- app/src/main/kotlin/com/siavash/dualcamera/ApplicationBase.kt
+++ app/src/main/kotlin/com/siavash/dualcamera/ApplicationBase.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.content.pm.PackageManager
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.answers.Answers
+import com.crashlytics.android.core.CrashlyticsCore
 import io.fabric.sdk.android.Fabric
 
 /**
@@ -20,7 +21,10 @@ class ApplicationBase : Application() {
         val appInfo = packageManager.getApplicationInfo(packageName, PackageManager.SIGNATURE_MATCH)
         appName = packageManager.getApplicationLabel(appInfo) as String
 
-        Fabric.with(this, Crashlytics());
+        val crashlyticsKit = Crashlytics.Builder()
+                .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build()
+        Fabric.with(this, crashlyticsKit);
         Fabric.with(this, Answers());
     }
 }
